b367869b4001c3770b293eeb4112ffba4f35529c
Samstiles-Dev <ayosamuel777@gmail.com>
2023-10-04 01:22:58 -0700

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-""" checks for lowercase character"""
 
+""" checks for lowercase character"""
 def islower(c):
     if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
