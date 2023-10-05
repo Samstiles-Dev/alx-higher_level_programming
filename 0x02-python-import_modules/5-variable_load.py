74bd23edce0e616a19bec771484acc320a95b2ec
Samstiles-Dev <ayosamuel777@gmail.com>
2023-10-05 02:06:19 -0700

    5-variable_load.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""imports the var a from the variable_load_5.py & prints its value"""
+
+
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print("{}".format(a))
