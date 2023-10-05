d099a25e0ce3ccbe440a1101c591fc5143a91e2a
Samstiles-Dev <ayosamuel777@gmail.com>
2023-10-05 01:36:45 -0700

    0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""program that prints the result of the addition 1 + 2 = 3"""
+
+
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
