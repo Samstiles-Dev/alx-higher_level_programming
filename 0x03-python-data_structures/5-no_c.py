95d5dffcaa976334dae58dd255d53e7977436c21
Samstiles-Dev <ayosamuel777@gmail.com>
2023-10-08 09:01:58 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Use list comprehension to create a new string without 'c' and 'C'"""
+"""Use list comprehension to create a new string without c and C"""
 
 
 def no_c(my_string):
-    new_string = ''.join(char for char in my_string if char.lower() not in {'c', 'C'})
+    new_string = ("".join(c for c in my_string if c not in "Cc"))
     return new_string
