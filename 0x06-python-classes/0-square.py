5ea2abd451fc312af7fc95bbf15673420b38395c
Samstiles-Dev <ayosamuel777@gmail.com>
2023-10-24 13:05:40 -0700

    empty square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,45 @@
 #!/usr/bin/python3
+"""Square module - Defines a simple Square class."""
+
 class Square:
-    pass
+    """
+    Square class represents a simple square.
+
+    Attributes:
+        side (int): The length of each side of the square.
+    """
+
+    def __init__(self, side=0):
+        """
+        Initializes a new Square with a specified side length.
+
+        Args:
+            side (int): The length of each side of the square.
+
+        """
+        self.side = side
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.side ** 2
+
+    def perimeter(self):
+        """
+        Calculates the perimeter of the square.
+
+        Returns:
+            int: The perimeter of the square.
+        """
+        return 4 * self.side
+
+if __name__ == "__main__":
+    my_square = Square(5)
+    print(f"{my_square.side}")
+    print(f"Area: {my_square.area()}")
+    print(f"Perimeter: {my_square.perimeter()}")
+
