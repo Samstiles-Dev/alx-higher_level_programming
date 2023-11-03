07a0b357cdc67abf389afa5c41e5470e7c6b4822
Samstiles-Dev <ayosamuel777@gmail.com>
2023-11-03 07:08:35 -0700

    102-python

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,28 +1,34 @@
-#include "Python.h"
+#include <stdio.h>
+#include <string.h>
+#include <Python.h>
 
 /**
- * print_python_string - this Prints info about Python strings.
- * @p: A PyObject string object.
+ * print_python_string - this func prints string info
+ * 
+ * @p: Python Object
+ * Return: no return
  */
 void print_python_string(PyObject *p)
 {
-	long int _len;
+	
+	PyObject *str, *_repr;
 
-	fflush(stdout);
-
+	(void)_repr;
 	printf("[.] string object info\n");
-	if (strcmp(p->ob_type->tp_name, "str") != 0)
+
+	if (strcmp(p->ob_type->tp_name, "str"))
 	{
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 
-	_len = ((PyASCIIObject *)(p))->_len;
-
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
-	printf("  _len: %ld\n", _len);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &_len));
+
+	_repr = PyObject_Repr(p);
+	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
+	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
+	printf("  value: %s\n", PyBytes_AsString(str));
 }
